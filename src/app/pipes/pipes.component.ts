c814242eff120963aa2d8e2d131aebe8911c5133
AntonyJr00 <antonyrodl20@gmail.com>
2023-01-07 11:34:03 -0500

    trabajo hecho

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,6 +1,12 @@
-import { IfStmt } from '@angular/compiler';
+import { IfStmt, ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
-
+interface Resp {
+  segundos: number;
+  minutos: number;
+  horas: number;
+  dias: number;
+  semanas: number;
+}
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
@@ -9,11 +15,23 @@ import { Component } from '@angular/core';
 export class PipesComponent {
   frase: string = '';
   valor: string = 'GBP';
-
-  tiempo = new Date('Jan 6, 2023, 6:33:51 PM');
-
-  tiempo2 = this.tiempo.toLocaleString();
-
-  after = new Date();
-  after2 = this.after.toLocaleString();
+  //
+  //tiempoPasado: Date = new Date('2022/12/29 09:42:57');
+  tiempoPasado: string = new Date().toISOString();
+  resp: Resp = {
+    segundos: 0,
+    minutos: 0,
+    horas: 0,
+    dias: 0,
+    semanas: 0,
+  };
+  resta: number = 0;
+  calcular() {
+    this.resta = new Date().getTime() - new Date(this.tiempoPasado).getTime();
+    this.resp.segundos = Math.round(this.resta / 1000);
+    this.resp.minutos = Math.floor(this.resp.segundos / 60);
+    this.resp.horas = Math.floor(this.resp.minutos / 60);
+    this.resp.dias = Math.floor(this.resp.horas / 24);
+    this.resp.semanas = Math.floor(this.resp.dias / 7);
+  }
 }
